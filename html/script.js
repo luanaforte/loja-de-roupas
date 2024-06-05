1fabdf967b13dedacd853e31506b1150f98d7caa
luanaforte <luanaforte950@gmail.com>
2024-06-05 14:54:40 -0300

    Adicionando pasta html oo repositório

---
+++ html/script.js
@@ -0,0 +1,339 @@
+let roupasFemininas = [
+    {
+        nome: 'Calça Casual Comfort',
+        imagem: '/03.imagensProdutos/feminino/roupas/f1.png',
+        qtd: 10,
+        descricao: 'Deslize para o conforto definitivo com nossa Calça Casual Comfort. Feita de uma mistura de algodão e elastano, esta calça se adapta perfeitamente ao seu corpo, proporcionando liberdade de movimento sem comprometer o estilo. Vista-se com confiança e elegância!',
+        preco: 49.99
+    },
+    {
+        nome: 'Camisa Essencial Preta',
+        imagem: './03.imagensProdutos/feminino/roupas/f2.png',
+        qtd: 15,
+        descricao: 'Faça uma declaração de moda clássica com nossa Camisa Essencial Preta. Confeccionada com algodão de alta qualidade, esta camisa oferece um ajuste perfeito e uma sensação luxuosa. Desde reuniões de negócios até noites na cidade, esta camisa é uma escolha versátil e sofisticada.',
+        preco: 29.99
+    },
+    {
+        nome: 'Casaco Elegância Atemporal',
+        imagem: './03.imagensProdutos/feminino/roupas/f3.png',
+        qtd: 8,
+        descricao: 'Entre na temporada com estilo e sofisticação usando nosso Casaco Elegância Atemporal. Criado com uma mistura de lã e poliéster, este casaco oferece conforto e calor sem sacrificar o estilo. Deixe sua marca com este clássico atemporal.',
+        preco: 79.99
+    },
+    {
+        nome: 'Short Jeans Vintage',
+        imagem: './03.imagensProdutos/feminino/roupas/f4.png',
+        qtd: 20,
+        descricao: 'Abraçando o espírito do vintage, nosso Short Jeans Vintage é uma adição indispensável ao seu guarda-roupa. Feito de denim durável, este short oferece conforto durante todo o dia, enquanto o design clássico adiciona um toque de estilo atemporal a qualquer look.',
+        preco: 39.99
+    },
+    {
+        nome: 'Camisa Manga Longa Luxo',
+        imagem: './03.imagensProdutos/feminino/roupas/f5.png',
+        qtd: 12,
+        descricao: 'Experimente o luxo da nossa Camisa Manga Longa Luxo. Feita com algodão egípcio de alta qualidade, esta camisa oferece uma sensação suave e sedosa contra a pele. Perfeita para o escritório ou ocasiões especiais, esta camisa é a definição de elegância.',
+        preco: 34.99
+    },
+    {
+        nome: 'Vestido Estampado Glamour',
+        imagem: './03.imagensProdutos/feminino/roupas/f6.png',
+        qtd: 18,
+        descricao: 'Faça uma entrada dramática em nosso Vestido Estampado Glamour. Fabricado em tecido de seda com uma estampa vibrante, este vestido é garantia de chamar a atenção onde quer que vá. Deixe sua personalidade brilhar com este vestido deslumbrante.',
+        preco: 59.99
+    },
+    {
+        nome: 'Conjunto Linho Frescor',
+        imagem: './03.imagensProdutos/feminino/roupas/f7.png',
+        qtd: 6,
+        descricao: 'Mantenha-se fresca e elegante em nosso Conjunto Linho Frescor. Feito de linho leve e respirável, este conjunto é perfeito para os dias ensolarados de verão. A elegância simples do design faz deste conjunto uma escolha perfeita para qualquer ocasião casual.',
+        preco: 69.99
+    },
+    {
+        nome: 'Camisa Malha Preta Essencial',
+        imagem: './03.imagensProdutos/feminino/roupas/f8.png',
+        qtd: 14,
+        descricao: 'Atualize seu guarda-roupa básico com nossa Camisa Malha Preta Essencial. Fabricada em malha de algodão premium, esta camisa oferece conforto inigualável e estilo sem esforço. Um essencial versátil que combina com qualquer estilo.',
+        preco: 27.99
+    },
+    {
+        nome: 'Short Jeans com Cinto Urban',
+        imagem: './03.imagensProdutos/feminino/roupas/f9.png',
+        qtd: 16,
+        descricao: 'Adicione um toque urbano ao seu visual com nosso Short Jeans com Cinto. Feito de denim de alta qualidade, este short oferece estilo e conforto para o dia a dia. O cinto adiciona um toque extra de estilo, garantindo que você se destaque da multidão.',
+        preco: 44.99
+    },
+    {
+        nome: 'Vestido Amarração Charme',
+        imagem: './03.imagensProdutos/feminino/roupas/f10.png',
+        qtd: 10,
+        descricao: 'Exiba seu charme feminino com nosso Vestido Amarração Charme. Feito de tecido leve e fluído, este vestido é perfeito para os dias quentes de verão. A amarração na cintura cria uma silhueta lisonjeira, enquanto o estilo descontraído o torna ideal para qualquer ocasião casual.',
+        preco: 54.99
+    },
+    {
+        nome: 'Saia Couro Elegância',
+        imagem: './03.imagensProdutos/feminino/roupas/f11.png',
+        qtd: 9,
+        descricao: 'Encante com nossa Saia Couro Elegância. Feita de couro macio e luxuoso, esta saia oferece uma mistura perfeita de sofisticação e estilo. Use com uma blusa simples para um look elegante ou com uma camiseta para um visual mais descontraído.',
+        preco: 37.99
+    },
+    {
+        nome: 'Blusa Linho Cropped Delicadeza',
+        imagem: './03.imagensProdutos/feminino/roupas/f12.png',
+        qtd: 11,
+        descricao: 'Explore a delicadeza do linho com nossa Blusa Linho Cropped. Fabricada com linho de alta qualidade, esta blusa oferece frescor e conforto durante todo o dia. O design cropped adiciona um toque moderno, perfeito para combinar com shorts ou saias de cintura alta.',
+        preco: 31.99
+    },
+    {
+        nome: 'Conjunto Pijama Curto Conforto',
+        imagem: './03.imagensProdutos/feminino/roupas/f13.png',
+        qtd: 7,
+        descricao: 'Desfrute de noites tranquilas e confortáveis com nosso Conjunto Pijama Curto Conforto. Feito de algodão macio e leve, este conjunto é perfeito para relaxar em casa ou para uma escapadela de fim de semana. Sinta o conforto e aconchego com este conjunto encantador.',
+        preco: 29.99
+    },
+    {
+        nome: 'Short Couro Marrom Elegância Urbana',
+        imagem: './03.imagensProdutos/feminino/roupas/f14.png',
+        qtd: 13,
+        descricao: 'Eleve seu estilo com nosso Short Couro Marrom Elegância Urbana. Fabricado em couro de alta qualidade, este short combina elegância e ousadia para criar um visual único. Use com uma blusa simples para um look casual ou com uma camisa para um visual mais polido.',
+        preco: 42.99
+    },
+    {
+        nome: 'Camisa Verde Babados Manga Delicadeza',
+        imagem: './03.imagensProdutos/feminino/roupas/f15.png',
+        qtd: 17,
+        descricao: 'Adicione um toque de delicadeza ao seu guarda-roupa com nossa Camisa Verde Babados Manga. Feita de algodão macio e leve, esta camisa oferece conforto durante todo o dia. Os babados nas mangas adicionam um toque feminino, perfeito para qualquer ocasião.',
+        preco: 36.99
+    },
+    {
+        nome: 'Vestido Verão Azul Brisa',
+        imagem: './03.imagensProdutos/feminino/roupas/f16.png',
+        qtd: 10,
+        descricao: 'Sinta a brisa do verão em nosso Vestido Verão Azul Brisa. Feito de tecido leve e fluído, este vestido é ideal para os dias quentes de verão. O azul vibrante e o design fluido criam uma aparência deslumbrante, perfeita para dias ensolarados e noites descontraídas.',
+        preco: 49.99
+    }
+];
+
+let produtosMasculinos = [
+    {
+        nome: 'Jaqueta Couro Preto',
+        imagem: '/03.imagensProdutos/masculino/roupas/m1.png',
+        qtd: 10,
+        descricao: 'Aventure-se com estilo em nossa Jaqueta Couro Preto. Feita de couro genuíno de alta qualidade, esta jaqueta oferece durabilidade e estilo atemporal. Com um design clássico e detalhes cuidadosamente trabalhados, esta jaqueta é perfeita para adicionar um toque de rebeldia a qualquer look.',
+        preco: 129.99
+    },
+    {
+        nome: 'Camisa Polo Cinza Moderna',
+        imagem: '/03.imagensProdutos/masculino/roupas/m2.png',
+        qtd: 15,
+        descricao: 'Elevando o básico a um novo patamar, nossa Camisa Polo Cinza Moderna é uma escolha versátil para qualquer ocasião. Feita de algodão macio e respirável, esta camisa oferece conforto durante todo o dia. O tom de cinza sutil e o design clássico garantem que você esteja sempre na moda.',
+        preco: 34.99
+    },
+    {
+        nome: 'Calça Jeans Reta Conforto',
+        imagem: '/03.imagensProdutos/masculino/roupas/m3.png',
+        qtd: 8,
+        descricao: 'Encare o dia com confiança em nossa Calça Jeans Reta Conforto. Feita de denim durável e elástico, esta calça oferece conforto e estilo sem sacrificar a liberdade de movimento. O corte reto clássico e a lavagem escura tornam esta calça uma escolha versátil para qualquer ocasião.',
+        preco: 49.99
+    },
+    {
+        nome: 'Camisa Básica Verde Elegância',
+        imagem: '/03.imagensProdutos/masculino/roupas/m4.png',
+        qtd: 20,
+        descricao: 'Adicione um toque de cor ao seu guarda-roupa com nossa Camisa Básica Verde Elegância. Feita de algodão macio e respirável, esta camisa oferece conforto durante todo o dia. O verde vibrante adiciona um toque de frescor a qualquer look, tornando esta camisa perfeita para todas as estações.',
+        preco: 24.99
+    },
+    {
+        nome: 'Calça Jogger Bege Urbana',
+        imagem: '/03.imagensProdutos/masculino/roupas/m5.png',
+        qtd: 12,
+        descricao: 'Mantenha-se confortável e estiloso em nossa Calça Jogger Bege Urbana. Feita de algodão macio e elástico, esta calça oferece conforto durante todo o dia. O design moderno e versátil faz desta calça uma escolha perfeita para qualquer look casual.',
+        preco: 39.99
+    },
+    {
+        nome: 'Calça Social Azul Escura Sofisticação',
+        imagem: '/03.imagensProdutos/masculino/roupas/m6.png',
+        qtd: 18,
+        descricao: 'Eleve seu estilo com nossa Calça Social Azul Escura Sofisticação. Feita de tecido de alta qualidade, esta calça oferece conforto e elegância para qualquer ocasião. O tom de azul escuro e o corte clássico garantem que você esteja sempre impecável.',
+        preco: 59.99
+    },
+    {
+        nome: 'Camisa Preta Básica Versatilidade',
+        imagem: '/03.imagensProdutos/masculino/roupas/m7.png',
+        qtd: 6,
+        descricao: 'Simplesmente elegante, nossa Camisa Preta Básica é uma peça essencial em qualquer guarda-roupa masculino. Feita de algodão macio e respirável, esta camisa oferece conforto durante todo o dia. O preto versátil combina facilmente com uma variedade de looks, tornando esta camisa perfeita para todas as ocasiões.',
+        preco: 29.99
+    },
+    {
+        nome: 'Camisa Social Branca Elegância',
+        imagem: '/03.imagensProdutos/masculino/roupas/m8.png',
+        qtd: 14,
+        descricao: 'Exude elegância com nossa Camisa Social Branca. Feita de algodão de alta qualidade, esta camisa oferece conforto e estilo sem igual. Perfeita para o escritório ou eventos formais, esta camisa é uma escolha versátil para o homem moderno.',
+        preco: 34.99
+    },
+    {
+        nome: 'Calça Social Preta Clássica',
+        imagem: '/03.imagensProdutos/masculino/roupas/m9.png',
+        qtd: 16,
+        descricao: 'Faça uma declaração de estilo com nossa Calça Social Preta Clássica. Feita de tecido de alta qualidade, esta calça oferece conforto e elegância para qualquer ocasião. O preto intenso e o corte clássico garantem que você esteja sempre impecável.',
+        preco: 49.99
+    },
+    {
+        nome: 'Calça Preta Jogger Urbana',
+        imagem: '/03.imagensProdutos/masculino/roupas/m10.png',
+        qtd: 10,
+        descricao: 'Mantenha-se confortável e estiloso em nossa Calça Preta Jogger Urbana. Feita de algodão macio e elástico, esta calça oferece conforto durante todo o dia. O design moderno e versátil faz desta calça uma escolha perfeita para qualquer look casual.',
+        preco: 44.99
+    },
+    {
+        nome: 'Camisa Xadrez Bege Casual',
+        imagem: '/03.imagensProdutos/masculino/roupas/m11.png',
+        qtd: 11,
+        descricao: 'Adicione um toque de estilo descontraído ao seu guarda-roupa com nossa Camisa Xadrez Bege Casual. Feita de algodão macio e respirável, esta camisa oferece conforto e estilo para qualquer ocasião. O padrão xadrez clássico adiciona um toque de personalidade a qualquer look.',
+        preco: 39.99
+    },
+    {
+        nome: 'Colete Preto Modernidade',
+        imagem: '/03.imagensProdutos/masculino/roupas/m12.png',
+        qtd: 17,
+        descricao: 'Acentue seu estilo com nosso Colete Preto Modernidade. Feito de tecido de alta qualidade, este colete oferece estilo e versatilidade para qualquer ocasião. Use sobre uma camisa social para um visual elegante ou sobre uma camiseta para um look casual.',
+        preco: 59.99
+    },
+    {
+        nome: 'Calça Jeans Clara Descontração',
+        imagem: '/03.imagensProdutos/masculino/roupas/m13.png',
+        qtd: 9,
+        descricao: 'Aproveite a descontração do fim de semana com nossa Calça Jeans Clara. Feita de denim leve e macio, esta calça oferece conforto e estilo para qualquer aventura. O tom claro adiciona um toque de frescor ao seu look casual.',
+        preco: 42.99
+    },
+    {
+        nome: 'Camisa Polo Azul Clara Frescor',
+        imagem: '/03.imagensProdutos/masculino/roupas/m14.png',
+        qtd: 13,
+        descricao: 'Mantenha-se fresco e estiloso em nossa Camisa Polo Azul Clara. Feita de algodão macio e respirável, esta camisa oferece conforto durante todo o dia. O azul claro suave adiciona um toque de frescor ao seu look casual.',
+        preco: 29.99
+    },
+    {
+        nome: 'Bermuda Jeans Descontração',
+        imagem: '/03.imagensProdutos/masculino/roupas/m15.png',
+        qtd: 15,
+        descricao: 'Curta o verão com nossa Bermuda Jeans. Feita de denim leve e macio, esta bermuda oferece conforto e estilo para os dias quentes. O corte moderno e versátil faz desta bermuda uma escolha perfeita para todas as suas aventuras.',
+        preco: 34.99
+    },
+    {
+        nome: 'Bermuda Jeans Azul Escura Despojada',
+        imagem: '/03.imagensProdutos/masculino/roupas/m16.png',
+        qtd: 20,
+        descricao: 'Aventure-se com estilo em nossa Bermuda Jeans Azul Escura Despojada. Feita de denim durável e elástico, esta bermuda oferece conforto e estilo para todas as ocasiões. O tom de azul escuro e o corte moderno tornam esta bermuda perfeita para qualquer aventura.',
+        preco: 39.99
+    }
+]
+
+
+let carrinhoCompras = []
+
+
+//Trasformamos toda a logica de montagem de produtos em uma função. Para passar o array e a div correta 
+//conseguimos generalizar a mesma função para a página de produtos femininos, masculinos ou infantis. 
+
+function montarProdutos(arrayProdutos, idDiv) {
+    // selecionando o elemento pai
+    let divRoupas = document.getElementById(idDiv); // div pai
+
+    // criando uma única div linha para conter todos os produtos
+    let divLinha = document.createElement('div'); // div filha
+    // adicionando a classe row
+    //inclusão das classes para display flexível 
+    divLinha.classList.add('row', 'd-flex', 'justify-content-center', 'align-items-center');
+
+    // adicionando o filho ao pai
+    divRoupas.appendChild(divLinha);
+
+    // iniciando o id para montar o identificador único de cada produto nos cards e modal de detalhes
+    let idProduto = 1;
+    arrayProdutos.forEach((produto) => {
+        // div card produto
+        let cardProduto = document.createElement('div');
+        // adicionando as classes card col m-3
+        cardProduto.classList.add('col-3', 'card', 'm-3');
+
+        cardProduto.innerHTML = `
+        <img src="${produto.imagem}" class="card-img-top my-3" 
+        alt="${produto.nome}">
+        <div class="card-body">
+            <h5 class="card-title">${produto.nome}</h5>
+            <p class="card-text"> ${produto.preco}</p>
+
+            <!-- inclusão de uma div para melhor disposição dos botões-->
+            <div class="d-flex justify-content-center align-items-center ">
+                <button class="btn btn-primary mx-1" data-bs-toggle="modal" data-bs-target="#masculino${idProduto}"> Ver detalhes</button>
+                <button onclick = " adicionarAoCarrinho(${produto})" class="btn btn-primary mx-1">Adicionar ao carrinho</button>  
+            </div>      
+        </div>
+        `;
+
+        // div linha sendo o elemento pai de cada card de produto.
+        divLinha.appendChild(cardProduto);
+
+        // criação do modal
+        let divModal = document.createElement('div');
+        divModal.innerHTML = `
+        <div class="modal fade" id="masculino${idProduto}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Detalhes Produto</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <!-- conteúdo no formato de tags -->
+            <!-- nome do produto -h5
+            descrição -p
+            preço- h6
+        -->
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="${produto.imagem}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="${produto.imagem}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="${produto.imagem}" class="d-block w-100" alt="...">
+            </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    
+            <h5>${produto.nome}</h5>
+            <p>${produto.descricao}</p>
+            <h6>R$ ${produto.preco}</h6>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            <button type="button" class="btn btn-primary">Adicionar ao carrinho</button>
+        </div>
+        </div>
+        </div>
+        </div>
+        `;
+
+        // Adicionando o modal ao final do divRoupas (fora da divLinha)
+        divRoupas.appendChild(divModal);
+        idProduto += 1;
+    });
+}
+
+
+function adicionarAoCarrinho(produto){
+    carrinhoCompras.push(produto)
+}
+
+montarProdutos(produtosMasculinos, 'roupasMasculinas');
